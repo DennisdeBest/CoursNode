ce673f6f4257ccee98e1399b226a4e59039063bf
DennisdeBest <dennis@debest.fr>
2016-10-07 20:14:20 +0200

    Scrap first paid article from sudouest.fr

--- project/scrapper.js
+++ project/scrapper.js
@@ -1,13 +1,20 @@
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: false })
+var nightmare = Nightmare({ show: true })
 
 nightmare
-  .goto('http://yahoo.com')
-  .type('form[action*="/search"] [name=p]', 'github nightmare')
-  .click('form[action*="/search"] [type=submit]')
-  .wait('#main')
+  .goto('http://sudouest.fr')
+  //.type('form[action*="/search"] [name=p]', 'github nightmare')
+  .click('.premium img')
+  .inject('js', `node_modules/jquery/dist/jquery.js`)
+  .wait('.long')
+  .screenshot('page.png')
   .evaluate(function () {
-    return document.querySelector('#main .searchCenterMiddle li a').href
+    if($('.long').text())
+    	return $('.long').text();
+    else
+        return "Not in page context";
+ 
+
   })
   .end()
   .then(function (result) {
