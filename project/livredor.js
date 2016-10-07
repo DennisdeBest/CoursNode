adf57adfe5903fae27877dbd92cd05fd12e1eb82
DennisdeBest <dennis@debest.fr>
2016-10-07 13:31:32 +0200

    Added nodemon global install to provision

--- project/livredor.js
+++ project/livredor.js
@@ -3,18 +3,18 @@ const qs = require('querystring');
 const db = require('sqlite');
 
 const FORM_PAGE = `
-    <html>
-    <body>
-    <h1>Coucou</h1>
-    <form method="POST" action="">
-    <label for="name">Name :</label>
-    <input id="name" name="name" type="text">
-    <label for="message">Message : </label>
-    <textarea id="message" name="message"></textarea>
-    <input type="submit" value="Envoyer" />
+<html>
+<body>
+<h1>Coucou</h1>
+<form method="POST" action="">
+<label for="name">Name :</label>
+<input id="name" name="name" type="text">
+<label for="message">Message : </label>
+<textarea id="message" name="message"></textarea>
+<input type="submit" value="Envoyer" />
 </form>
-    </body>
-    </html>
+</body>
+</html>
 `
 
 http.createServer((req, res) => {
@@ -24,22 +24,24 @@ http.createServer((req, res) => {
 
     res.write(FORM_PAGE);
 
-    if(req.method === 'POST'){
-            getPostData(req);
-        }
-res.end();
-}).listen(8080);
+    if (req.method === 'POST') {
+        getPostData(req);
+    }
+    res.end();
+}).
+listen(8080);
 
 function getPostData(req) {
     let body = "";
     req.on('data', (data) => {
         body += data
-        });
+    })
+    ;
 
     req.on('end', () => {
         let params = qs.parse(body);
-    console.log(params.name);
-    console.log(params.message);
-}
-)
+        console.log("Name : "+params.name);
+        console.log("Message : "+params.message);
+    }
+    )
 }
