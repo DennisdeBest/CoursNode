d707ffab37d5dbd2a6c9107348fb1ee4398be9d0
DennisdeBest <dennis@debest.fr>
2016-10-07 21:43:29 +0200

    Add article to database

--- project/scrapper.js
+++ project/scrapper.js
@@ -1,25 +1,47 @@
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true })
+var nightmare = Nightmare({ show: true });
+var db = require('sqlite');
 
-nightmare
-  .goto('http://sudouest.fr')
-  //.type('form[action*="/search"] [name=p]', 'github nightmare')
-  .click('.premium img')
-  .inject('js', `node_modules/jquery/dist/jquery.js`)
-  .wait('.long')
-  .screenshot('page.png')
-  .evaluate(function () {
-    if($('.long').text())
-    	return $('.long').text();
-    else
-        return "Not in page context";
- 
+db.open('sudquest.db').then(() => {
+	return db.run('CREATE TABLE if not exists infos (infos)')
+}).then(() => {
+	console.log('> DB created')
+}).catch((err) => {
+	console.log('ERR>', err)
+})
 
-  })
-  .end()
-  .then(function (result) {
-    console.log(result)
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error);
-  });
+function scrap() {
+	nightmare
+	.goto('http://sudouest.fr')
+	.click('.premium img')
+	.inject('js', `node_modules/jquery/dist/jquery.js`)
+	.wait('.long')
+	.screenshot('page.png')
+	.evaluate(function () {
+		if($('.long').text()) {
+			var output = $('.long').text();
+			return output;
+		}
+		else
+			return "Not in page context";
+
+	})
+	.end()
+	.then(function (result) {
+		console.log(result)
+		saveToDb(result)
+	})
+	.catch(function (error) {
+		console.error('Search failed:', error);
+	});
+
+}
+
+function saveToDb(input){
+	if (!input){
+		return Promise.reject(new Error("no input"));
+	}
+	return db.run("INSERT INTO infos VALUES (?)", input);
+};
+
+scrap()
