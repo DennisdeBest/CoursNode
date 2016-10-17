f28cd69229dd9e8dd51aba392b511f3677ef71a7
DennisdeBest <dennis@debest.fr>
2016-10-17 15:24:27 +0200

    GUI

--- project/index.js
+++ project/index.js
@@ -4,6 +4,16 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 8080;
 const API = require('json-api');
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'twig');
+
+// This section is optional and can be used to configure twig.
+app.set('twig options', { 
+	strict_variables: false
+});
+
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
@@ -16,7 +26,7 @@ db.open('expressapi.db').then(() => {
 	})
 
 	app.all('/', (req, res) => {
-		res.send('User api')
+		res.render('index', {message:"test"})
 	})
 
 	app.post('/users', (req, res) => {
@@ -37,22 +47,65 @@ db.open('expressapi.db').then(() => {
 			});
 		}
 		else {
-			db.each("SELECT rowid, * FROM users", function(err, row){
-				res.write(row.rowid + " - " + row.name + " - " + row.email+ "\n")
-			}).then(() => {
-				res.end();
-			});
+			db.all("SELECT rowid, * FROM users").then((users) => {
+				res.render('users/index', {users: users})
+			}).catch((err) => {
+				res.status(500).end();
+				console.log(err);
+			})
 		}
 
 	});
+	app.get('/users/add', (req, res) => {
+		console.log("add");
+		res.render("users/edit");
+	})
+	app.post('/users/add', (req, res) => {
+		var date =  new Date();
+		db.run("INSERT INTO users VALUES (?,?, ?, ?)", req.body.name, req.body.email, date, null).then(() => {
+			res.format({
+				html: function(){
+					res.redirect("/users")
+				}, 
+				json: function() {
+					res.send(user);
+					console.log("json");
+				}
+			});
+		})
+		console.log(req.body)
+	})
 	app.get('/users/:userid', (req, res) => {
 		db.get("SELECT rowid, * FROM users WHERE rowid = ?", req.params.userid).then((user) => {
-			res.send(user)
+			res.format({
+				html: function(){
+					res.render("users/show", {user: user});
+				}, 
+				json: function() {
+					res.send(user);
+					console.log("json");
+				}
+			});
 		}).catch((err) => {
 			res.status(500).end();
 			console.log(err);
 		});
 	})
+
+	app.post("/users/delete/:userid", (req, res) => {
+		db.run("DELETE FROM users WHERE rowid = ?",req.params.userid ).then(() => {
+			res.format({
+				html: function(){
+					res.redirect("/users")
+				}, 
+				json: function() {
+					res.send(user);
+					console.log("json");
+				}
+			});
+		})
+	})
+
 	app.put('/users/:userid', (req, res) => {
 		var date =  new Date();
 
